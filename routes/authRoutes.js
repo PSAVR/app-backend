518bf632ab17d3e9d0592769019bc0df7ed4a339
Emily-MM <65458318+Emily-MM@users.noreply.github.com>
2026-01-15 10:56:43 -0500

    Edicion de authRoutes para cookies

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,8 +16,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const cookieOpts = {
   httpOnly: true,
-  secure: isProd,                 
-  sameSite: isProd ? "none" : "lax",
+  secure: true,                 
+  sameSite:"none",
   path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000
 };
